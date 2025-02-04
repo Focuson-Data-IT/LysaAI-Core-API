d44b2f42ee6996d74c65e7a1d6088b0589b571aa
kevinirawand <kevinid07@gmail.com>
2025-02-04 15:53:37 +0700

    Init

---
+++ src/main/java/com/fosm/lysaai_core_api/LysaaiCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.fosm.lysaai_core_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LysaaiCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LysaaiCoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fosm/lysaai_core_api/LysaaiCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fosm.lysaai_core_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LysaaiCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
